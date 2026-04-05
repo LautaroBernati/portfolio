982a928e8def5ef2bc60a675a606fe849d231019
Lauti <lautarobernati@gmail.com>
2026-04-05 04:48:02 +0200

    v7.1.0: Home revamp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { startWith } from 'rxjs';
 import Typed, { TypedOptions } from 'typed.js';
 
@@ -21,39 +21,54 @@ import Typed, { TypedOptions } from 'typed.js';
 export class HomePage implements OnInit, OnDestroy {
   private readonly _destroyRef = inject(DestroyRef);
   private readonly _translateService = inject(TranslateService);
-  private typedInstance?: Typed;
+  private _fixedTypedInstahce?: Typed;
+  private _typedInstance?: Typed;
 
   constructor() { }
 
   ngOnDestroy(): void {
-    this.typedInstance?.destroy();
+    this._typedInstance?.destroy();
+    this._fixedTypedInstahce?.destroy();
   }
 
   ngOnInit(): void {
+    this._fixedTypedInstahce = this.createTyped(
+      '.fixed-typed',
+      ['', 'Component → Action → Reducer → Store → Selector → Component.', 'Action → Reducer → Effect → Action'],
+      3500
+    );
+
     this._translateService.onLangChange.pipe(
       startWith({ lang: this._translateService.currentLang }),
       takeUntilDestroyed(this._destroyRef),
     ).subscribe(value => {
       switch (value.lang) {
         case 'en':
-          this.createTyped(['', 'I am Full-Stack.', 'I build Web apps.', 'I build Mobile apps.']);
+          this._typedInstance = this.createTyped(
+            '.typed',
+            ['', 'Scalable, high-performance web apps.', 'Front-End expertise.', 'Reactive architecture.', 'Mobile-first approach.']
+          );
           break;
         case 'es':
-          this.createTyped(['', 'Soy Full-Stack.', 'Desarrollo Web apps.', 'Desarrollo Mobile apps.']);
+          this._typedInstance = this.createTyped(
+            '.typed',
+            ['', 'Web apps performantes y escalables.', 'Especialización en Front-End.', 'Arquitectura reactiva.', 'Enfoque mobile-first']
+          );
           break;
       }
+      this._typedInstance?.reset(true);
     });
   }
 
-  private createTyped(strings: string[]) {
-    this.typedInstance?.destroy();
+  private createTyped(cssName: string, strings: string[], backDelay = 2000) {
+    this._typedInstance?.destroy();
     const options: TypedOptions = {
       strings,
-      typeSpeed: 150,
-      backSpeed: 100,
-      loop: true
+      typeSpeed: 40,
+      backSpeed: 20,
+      loop: true,
+      backDelay,
     };
-    this.typedInstance = new Typed('.typed', options);
-    this.typedInstance.reset(true);
+    return new Typed(cssName, options);
   }
 }
